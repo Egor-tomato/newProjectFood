d0245a08fff963515d413916af0af3c03bb94218
Egor <marmeladov.egor@mail.ru>
2023-09-12 00:04:04 +0300

    realization answer modal

--- js/script.js
+++ js/script.js
@@ -154,5 +154,83 @@ window.addEventListener('DOMContentLoaded', () => {
          new MenuForm(img, altimg, title, descr, price, '.menu .container', 'menu__item').render();
       }));
 
+   const message = {
+      loading: 'img/form/spinner.svg',
+      success: 'Спасибо, мы с вами свяжемся',
+      failure: 'Что-то пошло не так'
+   }
+
+
+   const postData = async (url, data) => {
+      const res = await fetch(url, {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json'
+         },
+         body: data
+      });
+      return await res.json()
+   }
+
+   const forms = document.querySelectorAll('form');
+
+   forms.forEach(event => {
+      bindPostData(event);
+   });
+
+   function bindPostData(form) {
+      form.addEventListener('submit', event => {
+         event.preventDefault();
+
+         const statusMessage = document.createElement('img');
+         statusMessage.src = message.loading;
+         statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+         `;
+         form.insertAdjacent
+         Element('afterend', statusMessage);
+
+         const formData = new FormData(form);
+
+         const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+         postData('http://localhost:3000/requests', json)
+            .then(data => {
+               console.log(data);
+               thankModal(message.success);
+               statusMessage.remove();
+            })
+            .catch(() => {
+               thankModal(message.failure);
+            })
+            .finally(() => {
+               form.reset();
+            })
+      });
+   }
+
+   function thankModal(message) {
+      const previousModalDialog = document.querySelector('.modal__dialog');
+      previousModalDialog.classList.add('hide');
+      openModal();
+      const thankModalDialog = document.createElement('div');
+      thankModalDialog.classList.add('modal__dialog');
+      thankModalDialog.innerHTML = `
+         <div class="modal__content">
+            <div data-close class="modal__close">&times;</div>
+            <div class="modal__title">${message}</div>
+         </div>
+       `;
+      document.querySelector('.modal').append(thankModalDialog);
+
+      setTimeout(() => {
+         thankModalDialog.remove();
+         previousModalDialog.classList.add('show');
+         previousModalDialog.classList.remove('hide');
+         closeModal();
+      }, 4000);
+   }
+
 
 });
