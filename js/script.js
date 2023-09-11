5135b345801641ae4abe813ebd1c8a495b7caf2e
Egor <marmeladov.egor@mail.ru>
2023-09-11 19:26:24 +0300

    realization timer functional

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
       tabsContent[i].classList.add('show', 'fade');
       tabsContent[i].classList.remove('hide');
       tabs[i].classList.add('tabheader__item_active');
-   }
+   };
 
    tabsParent.addEventListener('click', event => {
       const target = event.target;
@@ -43,7 +43,7 @@ window.addEventListener('DOMContentLoaded', () => {
       modal.classList.add('show');
       modal.classList.remove('hide');
       document.body.style.overflow = 'hidden';
-      clearInterval(timeModalOpen);
+      clearTimeout(timeModalOpen);
    }
 
    function closeModal() {
@@ -66,7 +66,44 @@ window.addEventListener('DOMContentLoaded', () => {
       if (event.code === 'Escape') closeModal();
    });
 
+   const deadline = '2023-09-14';
 
+   function getAverageTime(endtime) {
+      const t = Date.parse(endtime) - Date.parse(new Date());
+      const days = Math.floor(t / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((t / 1000 / 60) % 60);
+      const seconds = Math.floor((t / 1000) % 24);
 
+      return {
+         'total': t,
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds
+      }
+   }
+
+   function setClock(selector, endtime) {
+      const timer = document.querySelector(selector);
+      const days = timer.querySelector('#days');
+      const hours = timer.querySelector('#hours');
+      const minutes = timer.querySelector('#minutes');
+      const seconds = timer.querySelector('#seconds');
+
+      const timerId = setInterval(updateTimer, 1000);
+
+      function updateTimer() {
+         const t = getAverageTime(endtime);
+         days.innerHTML = t.days;
+         hours.innerHTML = t.hours;
+         minutes.innerHTML = t.minutes;
+         seconds.innerHTML = t.seconds;
+
+         if (t.total <= 0) clearTimeout(timerId);
+      }
+   }
+
+   setClock('.timer', deadline);
 
 });
